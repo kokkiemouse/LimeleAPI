77bda6f07177aaee6bc9f1d1b58fc0b7d210534d
Riysan <67790884+Riysan@users.noreply.github.com>
2020-09-19 12:32:24 +0900

    Update main.php

--- PHP/main.php
+++ PHP/main.php
@@ -1,2 +1,9 @@
 <?php
-//API v3をリリースしましたら更新致します
+//LimeleAPI v2
+
+$url = 'https://smple.com'; //短縮したいurl
+$api_url = 'https://riyblog.shop/limele/api/v2/';
+$json = file_get_content($api_url.'?url='.$url);
+$res = json_decode($json);
+
+if($res['msg'] === '404')
