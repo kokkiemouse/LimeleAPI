2e2f7f6bc6fef8aa39384e326173737c4bc25734
Riysan <67790884+Riysan@users.noreply.github.com>
2020-09-19 14:52:57 +0900

    Main

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,7 +1,7 @@
 //LimeleAPI v2
 
 var pop = promt("短縮したいURLを入力してください","https://"); //ポップアップ
-/*if(pop === null){ exit; }
+if(pop === null){ exit; }
 
 var api_url = "https://riyblog.shop/limele/api/v2/?url=" + pop; //XHR
 var xhr = new XMLHttpRequest();
