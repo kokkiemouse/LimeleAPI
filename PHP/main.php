e8c629c13e50cc140f504e5a770d740394039509
Riysan <67790884+Riysan@users.noreply.github.com>
2020-09-19 12:51:45 +0900

    PHP サンプルコード

--- PHP/main.php
+++ PHP/main.php
@@ -1,9 +1,21 @@
 <?php
 //LimeleAPI v2
 
-$url = 'https://smple.com'; //短縮したいurl
-$api_url = 'https://riyblog.shop/limele/api/v2/';
-$json = file_get_content($api_url.'?url='.$url);
+$url = "https://smple.com"; //短縮したいurl
+$api_url = "https://riyblog.shop/limele/api/v2/";
+$json = file_get_content($api_url."?url=".$url);
 $res = json_decode($json);
 
-if($res['msg'] === '404')
+if($res["msg"] === "404"){ //エラー処理
+    echo "500";
+}else{
+    echo $res["ver"]."<br/>"; //APIバージョン
+    echo $res["msg"]."<br/>"; //ステータスメッセージ
+    echo $res["url"]."<br/>"; //短縮済みURL
+}
+
+/**
+ * LimeleAPI v2 リファレンス
+ * PHPサンプルコード
+ * 2020/9/19
+ */
